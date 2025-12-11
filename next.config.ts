d5cfaa507870238d2c25272cb7bca55f885d4bd4
Marc Pedrero <marcpedrero@MacBook-Pro-de-Adriana.local>
2025-12-10 21:29:44 -0500

    Fix Vercel build: Externalize c2pa-node and sharp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['c2pa-node', 'sharp'],
 };
 
 export default nextConfig;
